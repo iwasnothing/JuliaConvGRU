70952180aef2530218eed815591e51be4c695c75
matthew <iwasnothing@gmail.com>
2021-01-24 15:08:34 +0800

    dockerfile

--- run.jl
+++ run.jl
@@ -1,8 +1,30 @@
+using Pkg
+Pkg.update()
+
+
+pkgList = ["Sockets","DataFramesMeta","CSV","Flux","HTTP","JSON","DataFrames","Plots","TimeSeries","Hyperopt","JSON"]
+for pkg in pkgList
+    Pkg.add(pkg)  
+end
+
+
+using TimeSeries
+using DataFrames,DataFramesMeta
+using Dates
+using Statistics
+using Flux
+using Flux: @epochs
+using Plots
+using Hyperopt
+using JSON
+using HTTP
+
 push!(LOAD_PATH, "OsRSIConv/");
 using DataFrames
 using OsRSIConv
 push!(LOAD_PATH, "TradeAPI/");
 using TradeAPI
+
 
 list = ["FB","AAPL","AMZN","GOOG","NFLX","SQ","MTCH","AYX","ROKU","TTD"]
 
